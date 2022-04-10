b1585b47054377094f0be1a41b9108f310cd2927
Matus <matusino.sykora@gmail.com>
2022-04-10 17:48:29 +0200

    add JWT token filter

---
+++ src/main/java/com/sykora/socialnetworkapp/config/JwtTokenFilter.java
@@ -0,0 +1,42 @@
+package com.sykora.socialnetworkapp.config;
+
+import com.sykora.socialnetworkapp.service.IJwtProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final IJwtProvider jwtProvider;
+
+    public JwtTokenFilter(IJwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                     FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.split(" ")[1];
+            Authentication authentication = null;
+            try {
+                authentication = jwtProvider.getAuth(token);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (authentication != null) {
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
